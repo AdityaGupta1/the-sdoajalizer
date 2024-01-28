eae1a6231480eb5d6a7ac7b92cba194f55d8f907
Aditya Gupta <adityag@gmail.com>
2024-01-27 23:58:02 -0500

    simple window

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,22 @@
 
 #include <iostream>
 #include <cuda_runtime.h>
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw.h"
+#include "ImGui/imgui_impl_opengl3.h"
 
-int main(int argc, char* argv[]) {
+GLFWwindow* window;
+
+int width = 960;
+int height = 540;
+
+void errorCallback(int error, const char* description)
+{
+    fprintf(stderr, "%s\n", description);
+}
+
+int main(int argc, char* argv[]) 
+{
     std::cout << "welcome to sdoaj's america" << std::endl;
     std::cout << "------------------------------------------------------------" << std::endl;
 
@@ -24,5 +38,77 @@ int main(int argc, char* argv[]) {
     std::cout << "compute capability:  " << major << "." << minor << std::endl;
     std::cout << "------------------------------------------------------------" << std::endl;
 
+    glfwSetErrorCallback(errorCallback);
+
+    if (!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    window = glfwCreateWindow(width, height, "the sdoajalizer", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetCursorPosCallback(window, mousePositionCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        return -1;
+    }
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+    std::cout << "------------------------------------------------------------" << std::endl;
+
+    mainLoop();
+
     return 0;
+}
+
+void mainLoop()
+{
+    while (!glfwWindowShouldClose(window))
+    {
+
+        glfwPollEvents();
+
+        // TODO
+
+        glfwSwapBuffers(window);
+    }
+
+    //ImGui_ImplOpenGL3_Shutdown();
+    //ImGui_ImplGlfw_Shutdown();
+    //ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (action == GLFW_PRESS)
+    {
+        switch (key)
+        {
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, GL_TRUE);
+            break;
+        }
+    }
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+    // TODO
+}
+
+void mousePositionCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    // TODO
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 int main(int argc, char* argv[]);
+
+void mainLoop();
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void mousePositionCallback(GLFWwindow* window, double xpos, double ypos);
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
